ee8e14d4c136167087eb4154beec82ffcafabd40
Mamidi,Sai Prakash Reddy <s533725@ch140014m.nwmissouri.edu>
2019-02-14 13:35:18 -0600

    Initial Commit

---
+++ MultiViews/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MultiViews
+//
+//  Created by Mamidi,Sai Prakash Reddy on 2/14/19.
+//  Copyright © 2019 Mamidi,Sai Prakash Reddy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
