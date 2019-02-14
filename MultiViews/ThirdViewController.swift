795d77ee70bd9f4290958032ae4527d0daaaa4e5
Mamidi,Sai Prakash Reddy <s533725@ch140014m.nwmissouri.edu>
2019-02-14 13:48:43 -0600

    IOS Multi view pages

---
+++ MultiViews/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  MultiViews
+//
+//  Created by Mamidi,Sai Prakash Reddy on 2/14/19.
+//  Copyright © 2019 Mamidi,Sai Prakash Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
